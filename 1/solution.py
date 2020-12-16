12ab9eba874dd226f0b5c6b6ed21f66595809c02
Lukáš Vokráčko <lukas@vokracko.cz>
2020-12-16 23:45:52 +0100

    [#1] second part + readme

--- 1/solution.py
+++ 1/solution.py
@@ -3,5 +3,5 @@
 from itertools import combinations
 
 numbers = [int(line) for line in open("input")]
-results = [x*y for x, y in combinations(numbers, r=2) if x+y == 2020]
-print(results)
+print([x * y for x, y in combinations(numbers, r=2) if x + y == 2020])
+print([x * y * z for x, y, z in combinations(numbers, r=3) if x + y + z == 2020])
